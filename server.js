d4e9e7e9131721194ba93f143aa2c969373da8cc
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-22 22:06:25 +0545

    setup mongoose and mongodb

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 const port = 3000;
 
@@ -6,6 +8,17 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/contact", (req, res) => {
+  res.send("Hello Contact!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:5862@cluster0.kkzicei.mongodb.net/node-api?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log(error.message));
