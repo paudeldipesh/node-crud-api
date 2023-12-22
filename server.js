01db3bd88d1934897c7d4785d487f6ee43c3d50a
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-22 23:05:21 +0545

    create post request

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
+import { Product } from "./models/productModel.js";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/contact", (req, res) => {
-  res.send("Hello Contact!");
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+    res.send(req.body);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
