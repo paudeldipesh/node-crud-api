f08571d3db76eeaa90a64bfa4eb7cf0a8626e45f
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-23 07:41:37 +0545

    add functionality to get a single product

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.get("/products", async (req, res) => {
   }
 });
 
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
