0308e5999fb83dcfa3bb16197a2193d8b6a285dd
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-23 07:23:39 +0545

    create get request

--- server.js
+++ server.js
@@ -11,7 +11,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/product", async (req, res) => {
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
